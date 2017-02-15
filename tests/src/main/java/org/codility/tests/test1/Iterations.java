9937f7d33fe4e111feb191547cb3b5e686f2d039
Javier Sanchez <javier@latitude.Home>
2017-02-15 22:02:46 +0000

    first commit

---
+++ tests/src/main/java/org/codility/tests/test1/Iterations.java
@@ -0,0 +1,11 @@
+package org.codility.tests.test1;
+
+/**
+ * Created by javier on 15/02/17.
+ */
+public class Iterations {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
